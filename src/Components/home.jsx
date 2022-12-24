3736c6fe09535d07aebb15c28722f288300d5212
Mathias Krostewitz <mk@intutec.io>
2022-12-24 10:35:01 +0100

    iniital commit mid-term CAPStine Project

---
+++ src/Components/home.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import { FormBody } from '../Context/context';
+import Logo from '../Pictures/bank.png';
+
+function Home() {
+    return (
+            <FormBody
+                bgcolor="primary"
+                txtcolor="white"
+                header="Welcome to the Bad Bank"
+                title="Welcome to the bank"
+                text="We are the first bank who doesnt care about your privacy."
+                body={<img src={Logo} className="img-fluid" alt="Responsive image" />}
+            />
+    );
+};
+
+export default Home
